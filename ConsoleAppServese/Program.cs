6093b2b7edb2e234c7f3b36fca5b4567268084d6
Сергей Захаров <sergei86zahar@mail.ru>
2021-06-11 09:57:20 +0700

    1

---
+++ ConsoleAppServese/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppServese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyService.ServiceClient serviceClient = new MyService.ServiceClient();
+
+            string res = serviceClient.GetSum(10.ToString(), 20.ToString());
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
